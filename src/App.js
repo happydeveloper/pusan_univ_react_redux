d45e248037bcf338d08deb94a2dec3f86db2aa27
duru <enfn2001@gmail.com>
2020-01-30 21:38:41 +0900

    prop 배열과 맵 이용

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Header extends Component {
+  render() {
+    return (
+        <header>
+          <h1>{this.props.title}</h1>
+        </header>
+    );
+  }
+}
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          {this.props.items.map(function(ele){
+          return <li><a href={ele.id}>{ele.title}</a></li>
+          })}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Article extends Component {
+  render() {
+    return (
+      <article>
+        <h1>{this.props.title}</h1>
+        <p>
+          {this.props.desc}
+        </p>
+      </article>
+    );
+  }
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header title="WEB"></Header>
+        <Nav items={[
+                      {id:1, title:'HTML'},
+                      {id:2, title:'CSS'},
+                      {id:3, title:'JavaScript'}
+                    ]}></Nav>
+        <Article title="WEB" desc="WEB is ..."></Article>
     </div>
   );
 }
